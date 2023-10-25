812d4a26c6286ad2db3ce4b4fd310dab786ac847
cirospaciari <ciro.spaciari@gmail.com>
2023-10-24 22:36:48 -0300

    orelse

--- src/bun.js/api/bun/socket.zig
+++ src/bun.js/api/bun/socket.zig
@@ -919,8 +919,7 @@ pub const Listener = struct {
 
         globalObject.bunVM().eventLoop().ensureWaker();
 
-        const socket_context_result = uws.us_create_bun_socket_context(@intFromBool(ssl_enabled), uws.Loop.get().?, @sizeOf(usize), ctx_opts);
-        if (socket_context_result == null) {
+        const socket_context = uws.us_create_bun_socket_context(@intFromBool(ssl_enabled), uws.Loop.get().?, @sizeOf(usize), ctx_opts) orelse {
             const err = JSC.SystemError{
                 .message = bun.String.static("Failed to connect"),
                 .syscall = bun.String.static("connect"),
@@ -928,8 +927,7 @@ pub const Listener = struct {
             };
             exception.* = err.toErrorInstance(globalObject).asObjectRef();
             return .zero;
-        }
-        var socket_context = socket_context_result.?;
+        };
         var connection: Listener.UnixOrHost = if (port) |port_| .{
             .host = .{ .host = (hostname_or_unix.cloneIfNeeded(bun.default_allocator) catch unreachable).slice(), .port = port_ },
         } else .{
